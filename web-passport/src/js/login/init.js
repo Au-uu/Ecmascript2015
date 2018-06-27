62195372e8a0369934f4b35a00540f9cb6cfc353
Au-Labyrinth <13937376932@163.com>
2018-06-27 14:32:12 +0800

    fillup rest chapter

--- web-passport/src/js/login/init.js
+++ web-passport/src/js/login/init.js
@@ -1,7 +1,29 @@
-import render from './render.js';
-import event from './event.js';
+import '../common/polyfill';
+import render from './render';
+import bindEvent from './event';
 
-window.login = (opts) => {
-  const container = opts.container;
-  render(container);
+
+
+const login = (opts = {}) => {
+    var defaultOpts = {
+        loginBtnText: '登 录',
+        accountPlaceHolder: '手机号/邮箱/账号',
+        accountLabel: '',
+        passwordPlaceHolder: '请填写密码',
+        passwordLabel: '',
+        verifyPlaceHolder: '验证码',
+        accountMax: '30',
+        passwordMax: '30',
+        showRemember: true,
+        autocomplete: false,
+    };
+
+    var options = Object.assign(defaultOpts, opts)
+
+    render(options);
+    bindEvent(options);
 }
+
+export { login }
+
+// new FormCheck({});
